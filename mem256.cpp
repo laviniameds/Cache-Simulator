dcb57f8c63c026b83f9a23f558fbca234f4aa369
Lavinia Medeiros Miranda <laviniameds@icloud.com>
2019-08-11 00:04:05 -0300

    add trace

--- mem256.cpp
+++ mem256.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include<fstream>
 
 #define QTD_CACHE 8
 #define QTD_RAM 256
@@ -7,7 +8,7 @@
 using namespace std;
 
 struct CACHE {
-    int p[QTD_CACHE];
+    int p;
     string address;
     string data;
 };
@@ -17,31 +18,123 @@ struct RAM {
     string data;
 };
 
-int write_memory_data(RAM mem[QTD_RAM]){
+RAM mem[QTD_RAM];
+CACHE cache[QTD_CACHE];
+
+int write_memory_data(){
     //write memory data
     for(int i=0;i<QTD_RAM;i++)
-        cout << "ADD: " << mem[i].address << " DATA: " << mem[i].data << endl;
+        cout << "ADD: " << mem[i].address 
+        << " DATA: " << mem[i].data 
+        << endl;
 }
 
-void load_memory(RAM mem[QTD_RAM]){
+int write_cache_data(){
+    //write memory data
+    for(int i=0;i<QTD_CACHE;i++)
+        cout << "P: " << cache[i].p 
+        << " ADD: " << cache[i].address 
+        << " DATA: " << cache[i].data 
+        << endl;
+}
+
+void load_memory(){
+    ifstream myReadFile;
+    myReadFile.open("initial_data/initial_data");
+
+    if (!myReadFile) {
+        cout << "Unable to open file";
+        exit(1); // terminate with error
+    }
+
     //load memory
     for(int i=0;i<QTD_RAM;i++)
-        cin >> mem[i].address >> mem[i].data;
+        myReadFile >> mem[i].address >> mem[i].data;
+
+    myReadFile.close();
 }
 
-int main(){
-    RAM mem[QTD_RAM];
-    CACHE cache[QTD_CACHE];
-    
-    string address;
+void load_cache(){
+    for(int i=0;i<QTD_CACHE;i++){
+        cache[i].p = 1;
+        cache[i].address = "";
+        cache[i].data = "";
+    }
+}
+
+int search_memory(string address){
+    for(int i=0;i<QTD_RAM;i++){
+        if(address == mem[i].address)
+            return i;
+    }
+    return -1;    
+}
+
+int search_cache(string address){
+    for(int i=0;i<QTD_CACHE;i++){
+        if(address == cache[i].address)
+            return i;
+    }
+    return -1;
+}
+
+int get_min_p_cache(){
+    for(int i=0;i<QTD_CACHE;i++){
+        if(cache[i].p == 1)
+            return i;
+    }
+}
+
+void update_cache(int index){
+    cache[index].p = QTD_CACHE;
+
+    for(int i=0;i<QTD_CACHE;i++){
+        if(cache[i].p > 1 && cache[i].p != QTD_CACHE)
+            cache[i].p--;
+    }
+}
+
+void insert_cache(string address){
+    cache[get_min_p_cache()].address = address;
+    cache[get_min_p_cache()].data = mem[search_memory(address)].data;
+    update_cache(get_min_p_cache());
+}
+
+int main(){   
     
     //load memory
-    load_memory(mem);
+    load_memory();
 
+    //load_cache
+    load_cache();
+
+    //write_memory_data();
+    //write_cache_data();
+
+    double hit = 0;
+    double miss = 0;
+
+    string address;
+
+    // read trace
     while (cin >> address){
-        
+
+        int index = search_cache(address);
+
+        if(index != -1){
+            update_cache(index);
+            hit++;
+        }              
+        else {
+            insert_cache(address);
+            miss++;
+        }
     }
     
+    cout << "HITS: " << hit 
+    << " MISS: " << miss << endl
+    << "HIT RATE: " << 100*(hit/(hit+miss)) << "%"
+    << endl;
 
     return 0;
 }
---
+++ trace/generate_trace.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <iomanip>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+    for(int i=0;i<20;i++){
+        cout << showbase // show the 0x prefix
+        << internal // fill between the prefix and the number
+        << setfill('0'); // fill with 0s
+
+        cout << hex << uppercase << setw(4) << rand() % 5 << endl;
+    }
+
+    return 0;
+}
