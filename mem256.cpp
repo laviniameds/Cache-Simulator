05fbed5fed6a43cef5504a269cee094f7c738341
Lavinia Medeiros Miranda <laviniameds@icloud.com>
2019-08-10 21:16:42 -0300

    fix 'address' name

--- mem256.cpp
+++ mem256.cpp
@@ -8,28 +8,39 @@ using namespace std;
 
 struct CACHE {
     int p[QTD_CACHE];
-    string adress;
+    string address;
     string data;
 };
 
 struct RAM {
-    string adress;
+    string address;
     string data;
 };
 
-int main(){
-
-    RAM mem[QTD_RAM];
-    CACHE cache[QTD_CACHE];
-
-    //load memory
-    for(int i=0;i<QTD_RAM;i++)
-        cin >> mem[i].adress >> mem[i].data;
-    
-
+int write_memory_data(RAM mem[QTD_RAM]){
     //write memory data
     for(int i=0;i<QTD_RAM;i++)
-        cout << "ADD: " << mem[i].adress << " DATA: " << mem[i].data << endl;
+        cout << "ADD: " << mem[i].address << " DATA: " << mem[i].data << endl;
+}
+
+void load_memory(RAM mem[QTD_RAM]){
+    //load memory
+    for(int i=0;i<QTD_RAM;i++)
+        cin >> mem[i].address >> mem[i].data;
+}
+
+int main(){
+    RAM mem[QTD_RAM];
+    CACHE cache[QTD_CACHE];
+    
+    string address;
+    
+    //load memory
+    load_memory(mem);
+
+    while (cin >> address){
+        
+    }
     
 
     return 0;
